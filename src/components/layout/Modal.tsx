dc590939aa602bc43257e9bacbf84d0a52fa0fe2
Oungseik <mhemaungthuwin@gmail.com>
2022-10-02 11:36:14 +0630

    feat: create modal box for donation

---
+++ src/components/layout/Modal.tsx
@@ -0,0 +1,48 @@
+import clsx from 'clsx';
+import { useState } from 'react';
+
+export default function Modal() {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <>
+      <div className={clsx('fixed top-0 h-full w-full', !isOpen && 'hidden')}>
+        <div
+          className='absolute top-0 left-0 h-full w-full bg-gray-700/80 blur-sm'
+          onClick={closeModal}
+        />
+
+        <div className='absolute top-1/2 left-1/2 grid w-full max-w-[300px] -translate-y-1/2 -translate-x-1/2 gap-y-3 rounded-lg bg-zinc-300 px-3 py-4 text-gray-700'>
+          <h2 className='font-secondary text-3xl'>Monese</h2>
+
+          <p>
+            Lorem ipsum dolor sit amet, officia excepteur ex fugiat
+            reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit
+            ex esse exercitation amet. Voluptate laboris sint cupidatat ullamco.
+          </p>
+
+          <div className='flex gap-x-8 text-xs font-semibold'>
+            <a
+              href='https://www.google.com'
+              className='rounded-lg bg-indigo-700 px-3 py-2 text-gray-200'
+              target='_blank'
+              rel='noreferrer'
+            >
+              DONATE NOW
+            </a>
+            <button
+              className='rounded-lg border border-indigo-700 px-3 py-2 text-indigo-700'
+              onClick={closeModal}
+            >
+              NO THANK{' '}
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
