fb20a3542d765b4f088c000f802cf548bac1f9a1
Oungseik <mhemaungthuwin@gmail.com>
2022-10-02 16:35:29 +0630

    feat: create slider with swiper

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,74 +1,65 @@
 import * as React from 'react';
+import { Autoplay } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/autoplay';
+
+import featureData from '@/data/feature.json';
 
 import Layout from '@/components/layout/Layout';
-import ArrowLink from '@/components/links/ArrowLink';
-import ButtonLink from '@/components/links/ButtonLink';
-import UnderlineLink from '@/components/links/UnderlineLink';
-import UnstyledLink from '@/components/links/UnstyledLink';
+import Modal from '@/components/layout/Modal';
 import Seo from '@/components/Seo';
 
 /**
  * SVGR Support
- * Caveat: No React Props Type.
+ * Caveat: No React Props Type
  *
  * You can override the next-env if the type is important to you
  * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
  */
-import Vercel from '~/svg/Vercel.svg';
 
 // !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
 // Before you begin editing, follow all comments with `STARTERCONF`,
 // to customize the default configuration.
 
 export default function HomePage() {
+  const movies = featureData.movies;
+
   return (
     <Layout>
       {/* <Seo templateTitle='Home' /> */}
       <Seo />
-
-      <main>
-        <section className='bg-white'>
-          <div className='layout flex min-h-screen flex-col items-center justify-center text-center'>
-            <Vercel className='text-5xl' />
-            <h1 className='mt-4'>
-              Next.js + Tailwind CSS + TypeScript Starter
-            </h1>
-            <p className='mt-2 text-sm text-gray-800'>
-              A starter for Next.js, Tailwind CSS, and TypeScript with Absolute
-              Import, Seo, Link component, pre-configured with Husky{' '}
-            </p>
-            <p className='mt-2 text-sm text-gray-700'>
-              <ArrowLink href='https://github.com/theodorusclarence/ts-nextjs-tailwind-starter'>
-                See the repository
-              </ArrowLink>
-            </p>
-
-            <ButtonLink className='mt-6' href='/components' variant='light'>
-              See all components
-            </ButtonLink>
-
-            <UnstyledLink
-              href='https://vercel.com/new/git/external?repository-url=https%3A%2F%2Fgithub.com%2Ftheodorusclarence%2Fts-nextjs-tailwind-starter'
-              className='mt-4'
-            >
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
-                width='92'
-                height='32'
-                src='https://vercel.com/button'
-                alt='Deploy with Vercel'
-              />
-            </UnstyledLink>
-
-            <footer className='absolute bottom-2 text-gray-700'>
-              © {new Date().getFullYear()} By{' '}
-              <UnderlineLink href='https://theodorusclarence.com?ref=tsnextstarter'>
-                Theodorus Clarence
-              </UnderlineLink>
-            </footer>
-          </div>
-        </section>
-      </main>
+      <Swiper
+        modules={[Autoplay]}
+        className='mySwiper'
+        slidesPerView={1}
+        autoplay={{ delay: 5000 }}
+      >
+        {movies.map((movie) => (
+          <SwiperSlide key={movie.title}>
+            <div className='relative'>
+              <picture>
+                <source
+                  srcSet={`/images/movies/${movie.directory}/${movie.poster.large}`}
+                  type='image/jpg'
+                />
+                <img
+                  className='h-60 w-full object-cover object-top sm:h-80 md:h-[460px] lg:h-[480px]'
+                  src={`/images/movies/${movie.directory}/${movie.poster.large}`}
+                  alt={movie.title}
+                />
+              </picture>
+              <span
+                className='absolute bottom-0 text-lg font-bold'
+                style={{ textShadow: '1px 1px black' }}
+              >
+                {movie.title}
+              </span>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <Modal />
     </Layout>
   );
 }
