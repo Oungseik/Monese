02f838ccb808e3ec3abcf6ad65032a4008e1c540
Oungseik <mhemaungthuwin@gmail.com>
2022-10-10 10:18:39 +0630

    fix: hide hamburger menu in large screen

--- src/components/layout/HamburgerMenu.tsx
+++ src/components/layout/HamburgerMenu.tsx
@@ -19,12 +19,12 @@ export default function HamburgerMenu() {
   };
 
   return (
-    <div className='relative'>
+    <div className='relative sm:hidden'>
       <HamburgerButton isActive={isActive} onClick={onClickHandler} />
 
       <div
         className={clsx(
-          'absolute right-0 mt-1 flex w-44 flex-col gap-y-4 rounded-lg bg-gray-600 px-4 py-4 font-semibold',
+          'absolute right-0 z-10 mt-1 flex w-44 flex-col gap-y-4 rounded-lg bg-gray-600 px-4 py-4 font-semibold',
           !isActive && 'hidden'
         )}
       >
