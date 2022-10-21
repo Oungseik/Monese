35f1f4ec863c0069d83c7d5469a90a6826adf3eb
Oungseik <mhemaungthuwin@gmail.com>
2022-10-22 00:05:53 +0630

    feat: define type to be more generic

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,5 +1,4 @@
 export type PostType = 'movies' | 'series' | 'shorts';
-
 export type GetDirectory = (postType: PostType) => string;
 export type GetPath = (PostType: PostType) => (fileName: string) => string;
 
@@ -13,6 +12,8 @@ export type GetFileNames = (directory: string) => string[];
 
 export type GetId = (fileName: string) => string;
 
+export type GetPostData = (path: string) => MediaMetaDataSchema;
+
 type DownloadInfo = {
   server: 'google drive' | 'mega' | 'telegram';
   quality: '720p' | '1080p';
@@ -20,12 +21,24 @@ type DownloadInfo = {
   link: 'string';
 };
 
-export type MoviePostMetaDataSchema = {
+type Poster = {
+  small: string;
+  large: string;
+};
+
+// fields required in the markdown file
+export type MediaMetaDataSchema = {
+  id: string;
   title: string;
-  poster: string;
+  type: 'movie' | 'series';
+  poster: Poster;
   imdbRating: number;
   duration: string;
   genres?: string[];
   trailerUrl: string;
   DownloadInfos: DownloadInfo[];
 };
+
+export type MediaType = {
+  content: string;
+} & MediaMetaDataSchema;
