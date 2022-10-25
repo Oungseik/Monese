a9f0c8bd2f2f31ebf401a71c8d9ae33e6d05a2aa
Oungseik <mhemaungthuwin@gmail.com>
2022-10-25 14:44:49 +0630

    feat: create temporary section for ads

---
+++ src/components/layout/Ads.tsx
@@ -0,0 +1,8 @@
+export default function Ads() {
+  return (
+    <section className='mx-4 my-8 flex flex-col items-center justify-center border-2 border-gray-200 py-4'>
+      <h2>Ads available</h2>
+      <p>Contact us from our facebook page chat box </p>
+    </section>
+  );
+}
